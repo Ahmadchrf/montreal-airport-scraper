b6009c35c0f0d09803c15ee90944e4413e2b77ce
Ahmadchrf <ahmadcharaf6@gmail.com>
2025-01-09 11:52:05 -0500

    file created to easily test selenium access

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -2,11 +2,14 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-CHROME_DRIVER_PATH = r'C:\Users\ahmad\OneDrive\Bureau\chromedriver.exe'
+CHROME_DRIVER_PATH = "/chromedriver/chromedriver"
 
 options = Options()
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
+
 
 service = Service(executable_path=CHROME_DRIVER_PATH)
 driver = webdriver.Chrome(service=service, options=options)
